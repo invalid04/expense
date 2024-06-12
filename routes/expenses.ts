2fa9e29eaf2f1983dbb3c3f9961ce04279309939
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-12 01:15:24 -0500

    delete route created

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -26,6 +26,7 @@ export const expensesRoute = new Hono()
 .post("/", zValidator("json", createPostSchema), async (c) => {
     const expense = await c.req.valid("json")
     fakeExpenses.push({...expense, id: fakeExpenses.length + 1})
+    c.status(201)
     return c.json(expense)
 })
 .get("/:id{[0-9]+}", (c) => {
@@ -35,4 +36,13 @@ export const expensesRoute = new Hono()
         return c.notFound()
     }
     return c.json({expense})
+})
+.delete("/:id{[0-9]+}", (c) => {
+    const id = Number.parseInt(c.req.param('id'))
+    const index = fakeExpenses.findIndex((expense => expense.id === id))
+    if (index === -1) {
+        return c.notFound()
+    }
+    const deletedExpense = fakeExpenses.splice(index, 1)[0]
+    return c.json({ expense: deletedExpense })
 })
