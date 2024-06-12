0f3def8fb7118cf91e0dd3f2f1639200cf638530
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-12 02:23:55 -0500

    getting responses on the frontend from the backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,8 +11,13 @@ function App() {
   const [totalSpent, setTotalSpent] = useState(0)
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/expense/total-spent")
-  })
+    async function fetchTotal() {
+      const res = await fetch("/api/expenses/total-spent")
+      const data = await res.json()
+      setTotalSpent(data.total)
+    }
+    fetchTotal()
+  }, [])
 
   return (
     <Card className='w-[350px] m-auto'>
