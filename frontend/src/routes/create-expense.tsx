f14556c670b20e9c9ab0486da0d05f015e4c46f1
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-13 04:41:36 -0500

    create-expense ui

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,5 +1,28 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/create-expense')({
-  component: () => <div>Hello /create-expenses!</div>
+  component: CreateExpense,
 })
+
+function CreateExpense() {
+  return (
+    <div className='p-2'>
+      <h2>Create Expense</h2>
+      <form className='max-w-xl m-auto'>
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" id="title" placeholder="Title" />
+        <Label htmlFor="amount">Amount</Label>
+        <Input type="number" id="amount" placeholder="Amount" />
+        <Button 
+          type='submit'
+          className='mt-4'
+        >
+          Create Expense
+        </Button>
+      </form>
+    </div>
+  )
+}
