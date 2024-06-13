82e656f12bde3a13bb31c9c91a3fa297105cfcc1
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-13 05:12:08 -0500

    tanstack form setup

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,28 +1,95 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { useForm } from '@tanstack/react-form'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from '@/components/ui/button'
+
+import { api } from '@/lib/api'
 
 export const Route = createFileRoute('/create-expense')({
   component: CreateExpense,
 })
 
 function CreateExpense() {
+
+  const navigate = useNavigate()
+
+  const form = useForm({
+    defaultValues: {
+      title: '',
+      amount: 0,
+    },
+    onSubmit: async ({ value }) => {
+      const res = await api.expenses.$post({ json: value })
+
+      if(!res.ok) {
+        throw new Error('server error')
+      }
+      navigate({to: '/expenses'})
+    }
+  })
+
   return (
     <div className='p-2'>
       <h2>Create Expense</h2>
-      <form className='max-w-xl m-auto'>
-        <Label htmlFor="title">Title</Label>
-        <Input type="text" id="title" placeholder="Title" />
-        <Label htmlFor="amount">Amount</Label>
-        <Input type="number" id="amount" placeholder="Amount" />
-        <Button 
-          type='submit'
-          className='mt-4'
+        <form 
+          className='max-w-xl m-auto'
+          onSubmit={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+            form.handleSubmit()
+          }}
         >
-          Create Expense
-        </Button>
-      </form>
+
+          <form.Field 
+            name='title'
+            children={(field) => (
+              <>
+                <Label htmlFor={field.name}>Title</Label>
+                <Input 
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.touchedErrors ? (
+                  <em>{field.state.meta.touchedErrors}</em>
+                ) : null}
+              </>
+            )}
+          />
+
+          <form.Field 
+            name='amount'
+            children={(field) => (
+              <>
+                <Label htmlFor={field.name}>Amount</Label>
+                <Input 
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  type='number'
+                  onChange={(e) => field.handleChange(Number(e.target.value))}
+                />
+                {field.state.meta.touchedErrors ? (
+                  <em>{field.state.meta.touchedErrors}</em>
+                ) : null}
+              </>
+            )}
+          />
+
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+            children={([canSubmit, isSubmitting]) => (
+              <Button className='mt-4' type="submit" disabled={!canSubmit}>
+                {isSubmitting ? '...' : 'Submit'}
+              </Button>
+            )}
+          />
+
+        </form>
     </div>
   )
 }
