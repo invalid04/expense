cb23612e8d164e08c4f61855b0ea45a3d3cc7256
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-15 01:04:04 -0500

    auth setup

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/bun";
 import { expensesRoute } from "./routes/expenses";
+import { authRoute } from "./routes/auth";
 
 const app = new Hono()
 
@@ -9,6 +10,7 @@ app.use('*', logger())
 
 const apiRoutes = app.basePath("/api")
 .route("/expenses", expensesRoute)
+.route("/", authRoute)
 
 app.get('*', serveStatic({ root: './frontend/dist' }))
 app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
