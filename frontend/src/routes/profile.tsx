f51f5e41c04725cbecde008680cf02bbbe345a9b
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-15 01:27:08 -0500

    created profile page

---
+++ frontend/src/routes/profile.tsx
@@ -0,0 +1,36 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { useQuery } from '@tanstack/react-query'
+
+import { api } from '@/lib/api'
+
+export const Route = createFileRoute('/profile')({
+  component: Profile
+})
+
+async function getCurrentUser() {
+  const res = await api.me.$get()
+  if (!res.ok) {
+    throw new Error('server error')
+  }
+  const data = await res.json()
+  return data 
+}
+
+function Profile() {
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ['get-current-user'],
+    queryFn: getCurrentUser,
+  })
+
+  if (isPending) return 'loading'
+  if (error) return 'not logged in'
+
+  return (
+    <div className='p-2'>
+      <p>
+        {data.user.family_name}
+      </p>
+    </div>
+  )
+}
