9f590e77de6a69747033e77ede6564ac66b5e60e
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-15 04:52:54 -0500

    drizzle config file

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'drizzle-kit';
+import 'dotenv/config'
+import type { Config } from 'drizzle-kit'
+
+export default {
+  schema: './server/db/schema/*',
+  out: './drizzle',
+  driver: 'pg', 
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!
+  },
+};
--- server/db/index.ts
+++ server/db/index.ts
@@ -2,4 +2,4 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 const queryClient = postgres(process.env.DATABASE_URL!);
-const db = drizzle(queryClient);
+export const db = drizzle(queryClient);
