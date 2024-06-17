4015bf40cb5b19264a6d9f5b2a894d97815ce6cf
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-17 00:37:43 -0500

    updated getUser route to support db

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -4,6 +4,10 @@ import { z } from "zod";
 
 import { getUser } from "../kinde";
 
+import { db } from "../db";
+import {expenses as expenseTable } from "../db/schema/expenses"
+import { eq, desc, sum, and } from "drizzle-orm";
+
 const expenseSchema = z.object({
     id: z.number().int().positive().min(1),
     title: z.string().min(3).max(100),
@@ -14,15 +18,16 @@ type Expense = z.infer<typeof expenseSchema>
 
 const createPostSchema = expenseSchema.omit({ id: true })
 
-const fakeExpenses: Expense[] = [
-    { id: 1, title: "Groceries", amount: 50},
-    { id: 2, title: "Bills", amount: 120},
-    { id: 3, title: "Rent", amount: 450}
-]
-
 export const expensesRoute = new Hono()
-.get("/", getUser, (c) => {
-    return c.json({ expenses: fakeExpenses })
+.get("/", getUser, async (c) => {
+    const user = c.var.user 
+
+    const expenses = await db 
+        .select()
+        .from(expenseTable)
+        .where(eq(expenseTable.userId, user.id))
+    
+    return c.json({ expenses: expenses})
 })
 .post("/", getUser, zValidator("json", createPostSchema), async (c) => {
     const expense = await c.req.valid("json")
