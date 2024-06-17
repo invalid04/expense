bdd09b33f3dda15b9dc1383615a10c3bef3072d6
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-17 01:18:10 -0500

    added error messages to zod schema

--- server/sharedTypes.ts
+++ server/sharedTypes.ts
@@ -2,7 +2,10 @@ import { z } from "zod";
 
 export const expenseSchema = z.object({
     id: z.number().int().positive().min(1),
-    title: z.string().min(3).max(100),
+    title: z
+        .string()
+        .min(3, { message: 'Title must be at least 3 characters' })
+        .max(100, { message: 'Title must be at most 100 characters' }),
     amount: z.number().int().positive()
 })
 
